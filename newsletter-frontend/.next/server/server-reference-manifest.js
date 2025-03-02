b5ca916f69cc34093e7268ebb1a26ae220a12de8
nishantneupane <68480216+nishantneupane@users.noreply.github.com>
2025-03-02 02:41:41 -0500

    Separated Folders

---
+++ newsletter-frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CfEnpj7uIEmip20Ml4hJ+eIHTUaXZW2SxeJR2A+kTtg=\"\n}"
---
+++ newsletter-frontend/.next/static/chunks/pages_index_5771e187._.js
@@ -0,0 +1,18 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/pages_index_5771e187._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/pages_index_5771e187._.js",
+  "chunks": [
+    "static/chunks/node_modules_next_dist_compiled_bb1258e0._.js",
+    "static/chunks/node_modules_next_dist_shared_lib_92c54666._.js",
+    "static/chunks/node_modules_next_dist_client_b02cb968._.js",
+    "static/chunks/node_modules_next_dist_11228552._.js",
+    "static/chunks/node_modules_next_12ddfe43._.js",
+    "static/chunks/node_modules_react-dom_82bb97c6._.js",
+    "static/chunks/node_modules_1b7400a8._.js",
+    "static/chunks/[root of the server]__3759f8a9._.js"
+  ],
+  "source": "entry"
+});
